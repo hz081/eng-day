a91bf63c8d396e39b517623f62107086346d636c
Hantze <hantze@briohr.com>
2023-09-26 19:22:45 +0700

    update: some part of svc1

--- svc1/src/grpc-client.options.ts
+++ svc1/src/grpc-client.options.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
+    url: 'localhost:5001',
     package: 'sample_proto',
     protoPath: join(__dirname, './sample/sample.proto'),
   },
