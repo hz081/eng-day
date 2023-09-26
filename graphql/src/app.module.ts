424e6162c4f0afe28c93f53f030c683eeff7b85e
Hantze <hantze@briohr.com>
2023-09-26 18:36:50 +0700

    update: revert the grpc from graphql

--- graphql/src/app.module.ts
+++ graphql/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ConfigModule } from '@nestjs/config';
 import { SampleModule } from './modules/sample/sample.module';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -15,17 +15,6 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
     }),
     ConfigModule.forRoot({ isGlobal: true }),
     SampleModule,
-    ClientsModule.register([
-      {
-        name: 'sample_proto',
-        transport: Transport.GRPC,
-        options: {
-          url: 'localhost:5001',
-          package: 'sample_proto',
-          protoPath: '../../protobuf/modules/sample/sample.proto',
-        },
-      },
-    ]),
   ],
   controllers: [],
   providers: [],
--- graphql/src/modules/sample/infrastructure/sample.service.ts
+++ graphql/src/modules/sample/infrastructure/sample.service.ts
@@ -2,26 +2,27 @@ import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ISampleService } from '../services/sample.interface';
 import { Sample } from '../types/sample.type';
 import { ClientGrpc } from '@nestjs/microservices';
-import { SampleGrpcClient } from '../../../../../protobuf/modules/sample/sample';
+// import { SampleGrpcClient } from '../../../../../protobuf/modules/sample/sample';
 
 @Injectable()
-export class SampleService implements ISampleService, OnModuleInit {
-  private sampleGrpcClient: SampleGrpcClient;
+export class SampleService implements ISampleService {
+  // private sampleGrpcClient: SampleGrpcClient;
 
-  constructor(@Inject('sample_proto') private grpcClient: ClientGrpc) {}
+  // constructor(@Inject('sample_proto') private grpcClient: ClientGrpc) {}
 
-  onModuleInit() {
-    this.sampleGrpcClient =
-      this.grpcClient.getService<SampleGrpcClient>('SampleGrpc');
-  }
+  // onModuleInit() {
+  //   this.sampleGrpcClient =
+  //     this.grpcClient.getService<SampleGrpcClient>('SampleGrpc');
+  // }
 
   async getSample(param1: string, param2?: string): Promise<Sample> {
-    const response = await this.sampleGrpcClient
-      .getSample({ param1, param2: param2 ?? '' })
-      .toPromise();
-    return {
-      param1: response!.param1,
-      param2: response?.param2,
-    };
+    return { param1, param2 };
+    // const response = await this.sampleGrpcClient
+    //   .getSample({ param1, param2: param2 ?? '' })
+    //   .toPromise();
+    // return {
+    //   param1: response!.param1,
+    //   param2: response?.param2,
+    // };
   }
 }
